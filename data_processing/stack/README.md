46214ea8cf078c38912932d4c51176f8fee10bdf
Józef Piechaczek <244761@student.pwr.edu.pl>
2021-03-14 23:33:52 +0100

    Retrieve roles via regexes

--- data_processing/stack/README.md
+++ data_processing/stack/README.md
@@ -7,15 +7,42 @@ select *
 from Users
 where WebsiteUrl like '%github%'
 ```
-The query returned 30990 lines.
+The query returned 30990 rows.
 
 ## Processing data for collecting GitHub usernames
-- Removing lines with empty AboutMe section  
-    18514 lines remained.
+- Removing rows with empty AboutMe section  
+    18514 rows remained.
 - Applying regexes to retrieve GitHub usernames.
     ```
     https?:\/\/([^\/]+)\.github\.io.*
     https?:\/\/github.com\/([^\/]+)
     https?:\/\/www.github.com\/([^\/]+)
     ```
-    17970 lines remained.
+    17970 rows remained.
+
+## Processing data for determining technical roles
+- Extracting list of roles via regular expressions
+    ```
+    {
+        'name': 'Frontend',
+        'pattern': '.*front.{0,1}end.*'
+    },
+    {
+        'name': 'Backend',
+        'pattern': '.*back.{0,1}end.*'
+    },
+    {
+        'name': 'DevOps',
+        'pattern': '.*dev.{0,1}ops.*'
+    },
+    {
+        'name': 'DataScience',
+        'pattern': '.*data.{0,1}scientist.*'
+    },
+    {
+        'name': 'Mobile',
+        'pattern': '.*mobile.*'
+    }
+    ```
+- Removing rows with no roles  
+    1841 rows remained.
--- data_processing/stack/stack.py
+++ data_processing/stack/stack.py
@@ -2,10 +2,35 @@ import pandas as pd
 import re
 
 gh_username_patterns = [
-    "https?:\/\/([^\/]+)\.github\.io.*",
-    "https?:\/\/github.com\/([^\/]+)",
-    "https?:\/\/www.github.com\/([^\/]+)",
+    r'https?:\/\/([^\/]+)\.github\.io.*',
+    r'https?:\/\/github.com\/([^\/]+)',
+    r'https?:\/\/www.github.com\/([^\/]+)',
 ]
+
+role_patterns = [
+    {
+        'name': 'Frontend',
+        'pattern': '.*front.{0,1}end.*'
+    },
+    {
+        'name': 'Backend',
+        'pattern': '.*back.{0,1}end.*'
+    },
+    {
+        'name': 'DevOps',
+        'pattern': '.*dev.{0,1}ops.*'
+    },
+    {
+        'name': 'DataScience',
+        'pattern': '.*data.{0,1}scientist.*'
+    },
+    {
+        'name': 'Mobile',
+        'pattern': '.*mobile.*'
+    },
+]
+
+# re.match({"name": "DevOps", "pattern": r"\bdev.{0,1}ops\b"}["pattern"], "i am a devops someone", flags=re.IGNORECASE)
 
 
 def extract_user_name(patterns, url):
@@ -16,9 +41,23 @@ def extract_user_name(patterns, url):
     return None
 
 
+def extract_roles(roles, description):
+    detected_roles = []
+    for r in roles:
+        res = re.match(r['pattern'], description, flags=re.IGNORECASE)
+        if res is not None:
+            detected_roles.append(r['name'])
+    return detected_roles
+
+
 stack_data = pd.read_csv('./stack/data/QueryResults.csv')
 stack_data = stack_data[~stack_data.AboutMe.isnull()]
+
 stack_data['GithubUrl'] = stack_data.apply(
-    lambda row: extract_user_name(gh_username_patterns, row['WebsiteUrl']), axis=1)
+    lambda row: extract_user_name(gh_username_patterns, row['WebsiteUrl']),
+    axis=1)
 stack_data = stack_data[~stack_data.GithubUrl.isnull()]
-print()
+
+stack_data['Role'] = stack_data.apply(
+    lambda row: extract_roles(role_patterns, row['AboutMe']), axis=1)
+stack_data = stack_data[stack_data['Role'].apply(lambda x: len(x)) > 0]
